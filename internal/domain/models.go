aae8222326c357de39e4d56a13f4eefae1b02287
lipkerton <petr.petuxof@yandex.ru>
2026-06-11 16:01:38 +0300

    models

---
+++ internal/domain/models.go
@@ -0,0 +1,22 @@
+package domain
+
+import "time"
+
+type Thread struct {
+	ID		 	int
+	Subject  	string
+	Body 	 	string
+	Author 	 	string
+	Password	string
+	CreatedAt 	time.Time
+	BumpedAt 	time.Time
+}
+
+type Comment struct {
+	ID			int
+	ThreadID	int
+	Body 		string
+	Author		string
+	Password	string
+	CreatedAt	time.Time
+}
